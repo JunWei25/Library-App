3558a324b4a46b5bac435e897c1f18f66d7e83fe
Tan Jun Wei <junwei2509@gmail.com>
2025-02-01 23:35:58 +0800

    Added Library Services Question Forum

--- frontend/react-library/src/App.tsx
+++ frontend/react-library/src/App.tsx
@@ -14,6 +14,7 @@ import { LoginCallback, SecureRoute, Security } from "@okta/okta-react";
 import LoginWidget from './Auth/LoginWidget';
 import { ReviewListPage } from "./layouts/BooksCheckoutPage/ReviewListPage/ReviewListPage";
 import { ShelfPage } from "./layouts/ShelfPage/ShelfPage";
+import { MessagesPage } from "./layouts/MessagePage/MessagesPage";
 
 const oktaAuth = new OktaAuth(oktaConfig);
 
@@ -53,6 +54,7 @@ export const App = () => {
           <Route path='/login' render={() => <LoginWidget config={oktaConfig} /> }/>
           <Route path='/login/callback' component={LoginCallback}/>
           <SecureRoute path='/shelf'><ShelfPage/></SecureRoute>
+          <SecureRoute path='/messages'><MessagesPage/></SecureRoute>
         </Switch>
       </div>
       <Footer />
--- frontend/react-library/src/layouts/NavbarAndFooter/Navbar.tsx
+++ frontend/react-library/src/layouts/NavbarAndFooter/Navbar.tsx
@@ -18,7 +18,7 @@ export const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark main-color py-3">
       <div className="container-fluid">
-        <span className="navbar-brand">Luv 2 Read</span>
+        <span className="navbar-brand">Library</span>
         <button
           className="navbar-toggler"
           type="button"
